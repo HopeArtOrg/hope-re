d51b3ddd65ed5ddab9bf174c94cad3cc5ad8594a
Noah Trần <trananhquan1009@gmail.com>
2026-07-23 22:17:58 +0700

    feat(nix): add nix for better dev exp

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "HopeArtOrg/hope-re";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
+        devShells.default =
+          pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              wrapGAppsHook4
+              cargo
+              nodejs
+              rustc
+              pnpm
+            ];
+
+            buildInputs = with pkgs; [
+              librsvg
+              webkitgtk_4_1
+            ];
+
+            shellHook = "
+         export XDG_DATA_DIRS=$GSETTINGS_SCHEMAS_PATH
+        ";
+          };
+      };
+    };
+}
